82969d4fe1be3c0121c42360b739742543ae9c09
Fangzh-code <zhouheng.fang1989@gmail.com>
2019-09-02 01:14:32 +0200

    Function main

--- src/com/adneom/partition/Main.java
+++ src/com/adneom/partition/Main.java
@@ -1,10 +1,26 @@
 package com.adneom.partition;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		
+		Integer taille = 2;
+		List<List<Integer>> result = PartitionUtils.partition(list, taille);
+		result.forEach(System.out::print);
+		System.out.println("");
+		
+		taille = 3;
+		result = PartitionUtils.partition(list, taille);
+		result.forEach(System.out::print);
+		System.out.println("");
+		
+		taille = 1;
+		result = PartitionUtils.partition(list, taille);
+		result.forEach(System.out::print);
 	}
 
 }
