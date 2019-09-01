050c4ce9fed4c4be0ac9e943d866e79dbd54af9e
Fangzh-code <zhouheng.fang1989@gmail.com>
2019-09-02 00:14:29 +0200

    Init

---
+++ src/com/adneom/partition/Partition.java
@@ -0,0 +1,5 @@
+package com.adneom.partition;
+
+public class Partition {
+
+}
---
+++ src/com/adneom/test/PartitionTest.java
@@ -0,0 +1,19 @@
+package com.adneom.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PartitionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
